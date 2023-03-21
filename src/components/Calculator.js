41b7a4ed5bc6f97cdc9074b0fa922805b6277305
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-03-21 23:13:34 +0400

    Modify readme file and add child component to calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,13 @@
+import keys from "./keys";
 const Calculator = () => {
-  const arr = [
-    { btn: 'AC' },
-    { btn: '+/-' },
-    { btn: '%' },
-    { btn: '/' },
-    { btn: 7 },
-    { btn: 8 },
-    { btn: 9 },
-    { btn: '*' },
-    { btn: 4 },
-    { btn: 5 },
-    { btn: 6 },
-    { btn: '-' },
-    { btn: 1 },
-    { btn: 2 },
-    { btn: 3 },
-    { btn: '+' },
-    { btn: 0 },
-    { btn: '.' },
-    { btn: '=' },
-  ];
-  return (
+ return (
     <div className="container">
       <input type="text" readOnly placeholder="0" />
       <div className="keys">
-        {arr.map((element, index) => (<button id={index} key={element.btn} type="button">{element.btn}</button>))}
+        {keys().map((element, index) => (<button id={index} key={element.btn} type="button">{element.btn}</button>))}
       </div>
     </div>
   );
 };
+
 export default Calculator;
---
+++ src/components/keys.js
@@ -0,0 +1,25 @@
+const keys = ()=> {
+    const array = [
+        { btn: 'AC' },
+        { btn: '+/-' },
+        { btn: '%' },
+        { btn: '/' },
+        { btn: 7 },
+        { btn: 8 },
+        { btn: 9 },
+        { btn: '*' },
+        { btn: 4 },
+        { btn: 5 },
+        { btn: 6 },
+        { btn: '-' },
+        { btn: 1 },
+        { btn: 2 },
+        { btn: 3 },
+        { btn: '+' },
+        { btn: 0 },
+        { btn: '.' },
+        { btn: '=' },
+      ];
+      return (array)
+}
+export default keys
