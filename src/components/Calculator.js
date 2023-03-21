58795b02ffcdfc8d34902f1eff2f100d16bde1ba
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-03-21 23:27:14 +0400

    Fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,11 @@
-import keys from "./keys";
-const Calculator = () => {
- return (
-    <div className="container">
-      <input type="text" readOnly placeholder="0" />
-      <div className="keys">
-        {keys().map((element, index) => (<button id={index} key={element.btn} type="button">{element.btn}</button>))}
+import keys from './keys';
+
+const Calculator = () => (<div className='container'>
+      <input type='text' readOnly placeholder='0' />
+      <div className='keys'>
+        {keys().map((element, index) => (<button id={index} key={element.btn} type='button'>{element.btn}</button>))}
       </div>
     </div>
   );
-};
 
 export default Calculator;
