345d35c67c6750b8c032b2dd52012cf5c1ffd9b1
Leslie <Kooliesk@gmail.com>
2023-03-30 07:31:03 +0200

    Add more tests

--- src/routes/tests/operate.test.js
+++ src/routes/tests/operate.test.js
@@ -1,23 +1,45 @@
 import operate from '../../logic/operate';
 
 describe('Tests for operate.js', () => {
-  const add = jest.fn(() => operate(2, 2, '+'));
   test('if add function works', () => {
+    expect(operate(2, 2, '+')).toBe('4');
+    expect(operate(2.5, 1.5, '+')).toBe('4');
+    expect(operate(-2, 2, '+')).toBe('0');
+    expect(operate(0.1, 0.2, '+')).toBe('0.3');
+  });
 
-  }, expect(add()).toBe('4'));
-
-  const subtract = jest.fn(() => operate(2, 2, '-'));
   test('if subtract function works correctly', () => {
+    expect(operate(2, 2, '-')).toBe('0');
+    expect(operate(2.5, 1.5, '-')).toBe('1');
+    expect(operate(-2, 2, '-')).toBe('-4');
+    expect(operate(0.2, 0.1, '-')).toBe('0.1');
+  });
 
-  }, expect(subtract()).toBe('0'));
-
-  const divide = jest.fn(() => operate(2, 2, '÷'));
-  test('if divide function works correctly', () => {
-
-  }, expect(divide()).toBe('1'));
-
-  const multiply = jest.fn(() => operate(2, 2, 'x'));
   test('if multiply function works correctly', () => {
+    expect(operate(2, 2, 'x')).toBe('4');
+    expect(operate(2.5, 1.5, 'x')).toBe('3.75');
+    expect(operate(-2, 2, 'x')).toBe('-4');
+    expect(operate(0.1, 0.2, 'x')).toBe('0.02');
+  });
 
-  }, expect(multiply()).toBe('4'));
+  test('if divide function works correctly', () => {
+    expect(operate(2, 2, '÷')).toBe('1');
+    expect(operate(2.5, 1.5, '÷')).toBe('1.66666666666666666667');
+    expect(operate(-2, 2, '÷')).toBe('-1');
+    expect(operate(0.1, 0.2, '÷')).toBe('0.5');
+    expect(operate(2, 0, '÷')).toBe("Can't divide by 0.");
+  });
+
+  test('if modulo function works correctly', () => {
+    expect(operate(2, 2, '%')).toBe('0');
+    expect(operate(2.5, 1.5, '%')).toBe('1');
+    expect(operate(-2, 2, '%')).toBe('0');
+    expect(operate(0.1, 0.2, '%')).toBe('0.1');
+    expect(operate(2, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
+  });
+
+  test('if invalid operator returns error', () => {
+    expect(() => operate(2, 2, '#')).toThrow();
+  });
 });
+
